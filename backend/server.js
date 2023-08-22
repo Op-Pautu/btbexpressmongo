f105f24ca27e37e87c4ecd216235fc17b2582d9c
Op-Pautu <pautuop@gmail.com>
2023-08-22 23:14:29 +0530

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,8 @@ const app = express()
 app.get('/api/goals', (req, res) => {
     res.json({message: 'Get goals'})
 })
+app.get('/', (req, res) => {
+    res.json({message: 'welcome'})
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
